c88b3dc7531d968271bcc392abf46576a90f6195
AnBuiii <builehoaian2002@gmail.com>
2023-04-05 17:08:01 +0700

    login screen + signup screen

--- lib/src/presentation/login/screen/login_page.dart
+++ lib/src/presentation/login/screen/login_page.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_me/src/presentation/signup/screen/signup_page.dart';
 
 import '../../../controls/function/on_will_pop.dart';
+// import 'package:flutter/gestures.dart';
 import '../../../controls/function/route_function.dart';
 import '../../main/screen/main_page.dart';
 
@@ -13,6 +16,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   DateTime? currentBackPressTime;
+  final TextEditingController emailTextController = TextEditingController();
+  final TextEditingController passwordTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -23,18 +28,100 @@ class _LoginPageState extends State<LoginPage> {
           currentBackPressTime: currentBackPressTime,
         ),
         child: SafeArea(
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushReplacement(createRoute(
-                    screen: const MainPage(),
-                    begin: const Offset(0, 1),
-                  ));
-                },
-                child: const Text("Go to Home"),
-              )
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // image, app text
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 80, right: 100, left: 100),
+                  child: Image.asset("assets/images/Logoden.png"),
+                ),
+                const Text(
+                  "PhotoMe App",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                // email
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: TextField(
+                    controller: emailTextController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Email",
+                        prefixIcon: Icon(Icons.person)),
+                  ),
+                ),
+
+                //password
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: TextField(
+                    controller: passwordTextController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Password",
+                        prefixIcon: Icon(Icons.lock)),
+                  ),
+                ),
+
+                //login button
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: ElevatedButton(
+                    onPressed: () => {print("asd")},
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(16),
+                      child: const Text(
+                        "Sign in",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // forgot password
+
+                GestureDetector(
+                  onTap: () => {print("forgot password")},
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 40),
+                    child: Text(
+                      "Forgot password",
+                      style: TextStyle(color: Colors.blue, fontSize: 16),
+                    ),
+                  ),
+                ),
+
+                GestureDetector(
+                  onTap: () => {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SignupPage()),
+                    ),
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 32),
+                    child: Text(
+                      "Don't have an account? Create here ",
+                      style: TextStyle(color: Colors.blue, fontSize: 16),
+                    ),
+                  ),
+                ),
+
+                // dont have password
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/src/presentation/signup/screen/signup_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+// enum SingingCharacter { Ma }
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final TextEditingController emailTextController = TextEditingController();
+  final TextEditingController passwordTextController = TextEditingController();
+  int group = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create an account'),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: TextField(
+                controller: emailTextController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Email",
+                    prefixIcon: Icon(Icons.person)),
+              ),
+            ),
+
+            //password
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: TextField(
+                controller: passwordTextController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Password",
+                    prefixIcon: Icon(Icons.lock)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: TextField(
+                controller: passwordTextController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Re-password",
+                    prefixIcon: Icon(Icons.lock)),
+              ),
+            ),
+
+            Row(
+              children: [
+                Expanded(
+                  child: RadioListTile(
+                    value: 0,
+                    groupValue: group,
+                    onChanged: (value) => {},
+                    title: Text("Male"),
+                  ),
+                ),
+                Expanded(
+                  child: RadioListTile(
+                    value: 0,
+                    groupValue: group,
+                    onChanged: (value) => {},
+                    title: Text("Female"),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: ElevatedButton(
+                onPressed: () => {print("asd")},
+                child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(16),
+                  child: const Text(
+                    "Sign up",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  text: "By registering, you confirm that you accept our ",
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Term of service',
+                        style: TextStyle(color: Colors.amber)),
+                    TextSpan(text: ' and '),
+                    TextSpan(
+                        text: 'Privacy policy',
+                        style: TextStyle(color: Colors.amber)),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SignupPage()),
+                ),
+              },
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 32),
+                child: Text(
+                  "Don't have an account? Create here ",
+                  style: TextStyle(color: Colors.blue, fontSize: 16),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
